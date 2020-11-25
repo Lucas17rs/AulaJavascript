c0afa7f6b964547a42684bd28b98d74df2f7f21d
Lucas <lucasakajiri@hotmail.com>
2020-11-25 10:00:38 -0300

    condicionais, lacos de repeticao e date

--- js/main.js
+++ js/main.js
@@ -1,8 +1,38 @@
 
+var d = new Date();
+alert(d.getDay());
+alert(d.getHours());
+alert(d.getMinutes());
+
+/*
+var count;
+for(count=0; count<=5; count++){
+	alert(count);
+};
+*/
+
+/*
+var count =0;
+while(count <= 5){
+	console.log(count);
+	count++;
+};
+*/
+
+/*
+var idade = prompt("Qual a sua idade");
+if (idade>=18){
+	alert("maior de idade");
+}else{
+	alert("menor de idade");
+};
+*/
+
+/*
 var frutas = [{nome:"maça", cor:"vermelha"},{nome:"uva", cor:"roxa"} ];
 console.log(frutas)
 alert(frutas[1].nome);
-
+*/
 
 //var lista =["maça", "pera", "laranja"];
 //lista.push("uva");
