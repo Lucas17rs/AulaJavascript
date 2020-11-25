9348808fd8aad3d79475476a0a2604367e4927e7
Lucas <lucasakajiri@hotmail.com>
2020-11-25 09:39:40 -0300

    array e dicionario

--- js/main.js
+++ js/main.js
@@ -1,13 +1,25 @@
-/**
- * 
- */
-var nome = "Lucas ";
-var n1 = 5;
-var n2 = 3;
-var frase = "bom dia";
+
+var frutas = [{nome:"maça", cor:"vermelha"},{nome:"uva", cor:"roxa"} ];
+console.log(frutas)
+alert(frutas[1].nome);
+
+
+//var lista =["maça", "pera", "laranja"];
+//lista.push("uva");
+//lista.pop();
+//console.log(lista.length);
+//console.log(lista.reverse());
+//console.log(lista);
+//console.log(lista.toString());
+//console.log(lista.join(" - "));
+
+//var nome = "Lucas ";
+//var n1 = 5;
+//var n2 = 3;
+//var frase = "bom dia";
 //alert (nome + " tem " + idade);
 //alert (idade + idade2);
-console.log(nome);
-console.log(n1*n2);
-console.log(frase.toLocaleUpperCase);
+//console.log(nome);
+//console.log(n1*n2);
+//console.log(frase.toLocaleUpperCase);
 //alert (frase.replace("bom dia","boa noite"));
