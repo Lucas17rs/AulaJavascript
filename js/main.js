c16c965a7257f13bcd5a8efdc824ac77efa94a71
Lucas <lucasakajiri@hotmail.com>
2020-11-25 09:12:09 -0300

    introducao ao javaScript

---
+++ js/main.js
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+var nome = "Lucas ";
+var n1 = 5;
+var n2 = 3;
+var frase = "bom dia";
+//alert (nome + " tem " + idade);
+//alert (idade + idade2);
+console.log(nome);
+console.log(n1*n2);
+console.log(frase.toLocaleUpperCase);
+//alert (frase.replace("bom dia","boa noite"));
